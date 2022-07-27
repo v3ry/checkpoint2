8c18c9feadbb5ef3e4202707fed891cce76d0144
v3ry <v3ryfr@gmail.com>
2022-07-27 10:02:57 +0200

    adding axios and state

--- frontend/src/pages/CupcakeList.jsx
+++ frontend/src/pages/CupcakeList.jsx
@@ -1,10 +1,19 @@
 import Cupcake from "@components/Cupcake";
+import React,{useState,useEffect} from "react";
+import axios from "axios";
 
 export default function CupcakeList() {
+  const [apiData, setApiData] = useState([])
   // Step 1: get all cupcakes
-
+  useEffect(() => {
+    axios
+    .get(`http://localhost:4000/cupcakes`)
+    .then(data => setApiData(data.data))
+    //gestion des erreurs
+    .catch(error => console.warn('Authorization failed : ' + error.message));
+    },[])
   // Step 3: get all accessories
-
+  apiData && console.log(apiData);
   return (
     <>
       <h1>My cupcakes</h1>
